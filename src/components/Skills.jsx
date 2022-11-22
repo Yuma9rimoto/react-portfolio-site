3385867fc8dd01318c1d88e031227047691db4f1
YumaKurimotoDev <yuma.kurimoto.developer@gmail.com>
2022-11-22 18:58:23 +0900

    5-3まで

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export const Skills = () => {
+  const [languages, setLanguages] = useState([]);
+  console.log(languages);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/Yuma9rimoto/repos")
+      .then((response) => {
+        const languages = response.data.map((res) => res.language);
+        const countedLanguages = generateLanguageCountObj(languages);
+        setLanguages(countedLanguages);
+      });
+  }, []);
+
+  const generateLanguageCountObj = (allLanguageList) => {
+    const notNullLanguageList = allLanguageList.filter(
+      (language) => language != null
+    );
+    const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+    return uniqueLanguageList.map((item) => {
+      return {
+        language: item,
+        count: allLanguageList.filter((language) => language === item).length,
+      };
+    });
+  };
   return (
     <div id="skills">
       <div className="container">
